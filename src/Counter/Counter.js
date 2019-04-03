52f1f50ebfcd5f9f91cd0477bd8ce8cda7b69dea
mrozo2m <mrozo2m@interia.pl>
2019-04-03 11:27:47 +0200

    arrow function as a method

---
+++ src/Counter/Counter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Counter extends React.Component{
+    constructor(){
+        super()
+
+        this.state = {
+            number: 10,
+        }
+
+        this.inc = this.inc.bind(this)
+    }
+
+    inc = () => this.setState({ number: this.state.number + 1 })
+
+    render(){
+        return(
+            <div>
+                 <h1>
+                    {this.state.number}
+                </h1>
+
+                <button
+                    onClick={this.inc}
+                >
+                    +
+                </button>
+            </div>
+        )
+    }
+}        
+
+export default Counter
