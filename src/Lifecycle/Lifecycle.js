c2673d0629d4ecf8884edc0543aa1584d16d01ad
mrozo2m <mrozo2m@interia.pl>
2019-04-03 13:39:43 +0200

    DidMount component

---
+++ src/Lifecycle/Lifecycle.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Lifecycle extends React.Component {
+    constructor(){
+        super()
+
+        console.log ('constructor')
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')
+    }
+
+    render(){
+        console.log('render')
+
+        return(
+            <div>
+                Lifecycle
+            </div>
+        )
+    }
+}
+export default Lifecycle
